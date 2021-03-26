50d6547961431165ddc75ab18d30a98d4339330f
nicholaskier <nicholaskier@gmail.com>
2021-03-26 16:11:31 -0500

    first commit

---
+++ errors.py
@@ -0,0 +1,17 @@
+print(1)
+int(9)
+# int 9
+# python functions need parenthesis
+print(2)
+# print 3
+# python functions need parenthesis
+a = [1,2,3]
+# syntax syntax syntax
+# exceptions
+# RUNTIME ERRORS
+a = 1
+b = '2'
+print(int(2.5))
+# closing parenthesis 
+print(a + b)
+# trying to add an integer and a string
